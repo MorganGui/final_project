f7c4fa1a62eb8183df110ead116ea888684ebc89
MorganGui <90325277+MorganGui@users.noreply.github.com>
2023-04-07 14:08:20 +0200

    make form

---
+++ src/Form/MessageType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Message;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MessageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('content')
+            ->add('created_at')
+            ->add('user')
+            ->add('subject')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Message::class,
+        ]);
+    }
+}
